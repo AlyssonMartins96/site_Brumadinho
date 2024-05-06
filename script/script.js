6a307b15d27966bcf88ae6eac5ced5a1d7d1b242
Alysson Martins <alysson96.96@hotmail.com>
2024-05-05 21:26:18 -0300

    AlteraçãoMenuMobile

--- script/script.js
+++ script/script.js
@@ -1,10 +1,8 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+function clickMenu() {
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+    itens.style.display = 'block'
     }
+
 }
